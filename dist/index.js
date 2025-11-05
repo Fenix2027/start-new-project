a1d107f3b059134150c7776cdd75996f0088416a
Konstantsin <k4164724@gmail.com>
2025-11-05 12:25:26 +0300

    Commit -1

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    let helloMessage = 'Incubator1!';
+    res.send(helloMessage);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
