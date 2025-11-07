af59dcec423f5f53a44a02ba9ab16916d5fb5299
Konstantsin <k4164724@gmail.com>
2025-11-07 17:50:40 +0300

    Commit -5

--- dist/index.js
+++ dist/index.js
@@ -6,13 +6,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+const db = {
+    courses: [{ id: 1, title: 'front-end' },
+        { id: 2, title: 'beck-end' },
+        { id: 3, title: 'automtion qa' },
+        { id: 4, title: 'devops' }]
+};
 app.get('/courses', (req, res) => {
-    res.json([
-        { id: 1, title: 'front-end' },
-        { id: 1, title: 'beck-end' },
-        { id: 1, title: 'automtion qa' },
-        { id: 1, title: 'devops' }
-    ]);
+    let foundCourses = db.courses;
+    if (req.query.title) {
+        foundCourses = foundCourses
+            .filter(c => c.title.indexOf(req.query.title) > -1);
+    }
+    res.json(foundCourses);
+});
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(foundCourse);
+});
+app.post('/courses', (req, res) => {
+    const createdCourse = {
+        id: +(new Date()),
+        title: 'unknown'
+    };
+    db.courses.push(createdCourse);
+    res.json(createdCourse);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,40 @@
-import express, {Request,Response} from 'express'
+import express  from 'express'
+import {runInNewContext} from "node:vm";
 const app = express()
 const port = 3000
 
-app.get('/courses', (req: Request, res: Response) => {
-  res.json([
-      {id: 1, title:'front-end'},
-      {id: 2, title:'beck-end'},
-      {id: 3, title:'automtion qa'},
-      {id: 4, title:'devops'}
-  ])
+const db = {
+    courses: [ {id: 1, title: 'front-end'},
+        {id: 2, title: 'beck-end'},
+        {id: 3, title: 'automtion qa'},
+        {id: 4, title: 'devops'}]
+}
+
+app.get('/courses', (req, res) => {
+    let foundCourses = db.courses;
+      if(req.query.title) {
+          foundCourses = foundCourses
+              .filter(c => c.title.indexOf(req.query.title as string) > -1)
+      }
+    res.json(foundCourses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404)
+        return;
+    }
+    res.json(foundCourse)
+})
+
+app.post('/courses', (req, res) => {
+    const createdCourse = {
+        id: +(new Date()),
+        title: 'unknown'
+    };
+    db.courses.push(createdCourse)
+    res.json(createdCourse)
 })
 
 app.listen(port, () => {
