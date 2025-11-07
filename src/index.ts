88ecc62307cc84675f529de6a7a01d5285362c88
Konstantsin <k4164724@gmail.com>
2025-11-07 17:08:36 +0300

    Commit -4

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ const port = 3000
 app.get('/courses', (req: Request, res: Response) => {
   res.json([
       {id: 1, title:'front-end'},
-      {id: 1, title:'beck-end'},
-      {id: 1, title:'automtion qa'},
-      {id: 1, title:'devops'}
+      {id: 2, title:'beck-end'},
+      {id: 3, title:'automtion qa'},
+      {id: 4, title:'devops'}
   ])
 })
 
